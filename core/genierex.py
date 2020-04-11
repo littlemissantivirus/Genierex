3a3b2d9e074fe7d3ca102495faf1c277d85ad52b
littlemissantivirus <littlemissantivirus@gmail.com>
2020-04-10 22:06:44 -0400

    Use a custom logger over print()

--- core/genierex.py
+++ core/genierex.py
@@ -1,7 +1,8 @@
 from discord.ext import commands
 import discord
-import json
 import jishaku
+import json
+import logging
 
 class Genierex(commands.Bot):
 
@@ -11,6 +12,16 @@ class Genierex(commands.Bot):
         self.admins: list = json.load(open('data/configs/admins.json'))
         self.config: dict = json.load(open('data/configs/config.json'))
 
+        logging.basicConfig(filename='logs/genierex.log', level=logging.INFO,
+                            format='[%(asctime)s %(levelname)s] %(message)s', datefmt="%H:%M:%S")
+        self.logger = logging.getLogger('Genierex')
+        
+        console = logging.StreamHandler()
+        console.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('[%(asctime)s %(levelname)s] %(message)s', datefmt="%H:%M:%S")
+        console.setFormatter(formatter)
+        self.logger.addHandler(console)
+
         self.load_extensions()
     
     def load_extensions(self):
@@ -18,6 +29,6 @@ class Genierex(commands.Bot):
         for ext in extensions:
             try:
                 self.load_extension(ext)
-                print(f"Loaded {ext[5:]}!")
+                self.logger.info(f"Loaded {ext[5:]}!")
             except Exception as ex:
-                print(f"Was unable to load the extension {ext}!\nStacktrace:{ex}")
+                self.logger.warn(f"Was unable to load the extension {ext}!\nStacktrace:{ex}")
